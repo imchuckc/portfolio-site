10595acc9b7f0eb5823cf30d98da0acb303c0897
imchuckc <im.chuckc@gmail.com>
2025-01-26 21:22:11 +0800

    fix: add basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/portfolio-site',
+  assetPrefix: '/portfolio-site'
 };
 
 export default nextConfig;
